5e10510fb2e1e740af964926ab5613c9e3622b1b
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-18 11:44:27 -0700

    main.h

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,15 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
-	{
-		-putchar(48);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		-putchar(45);
 		return (-1);
+	}
+	else
+	{
+		-putchar(48);
+		return (0);
 	}
 	_putchar('\n');
 }
