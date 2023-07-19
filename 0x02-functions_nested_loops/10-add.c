9747beb1e83a592661c7cb8ec7eb131af95c3292
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-19 03:57:03 -0700

    main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,5 @@
+#include "main.h"
+/**
+ * add - add two integers and returns the result
+ * return: return result wich is integer
+ */
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 
 			if (p < 10)
 			{
-				_putchar('0' + p);
+				_putchar(p + '0');
 			}
 			else
 			{
