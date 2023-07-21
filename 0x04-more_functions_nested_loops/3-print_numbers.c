ced9380cb639e1675918caec49b38bc1853cef49
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-21 01:35:10 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,10 @@
 #include "main.h"
+#include <unistd.h>
+
 /**
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
- * Return: no return
+ * Return: void
  */
 void print_numbers(void)
 {
