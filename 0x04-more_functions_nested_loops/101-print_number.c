a2a5e9c6d729fbd295390e2e0b9d2248cccc733a
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-21 13:13:00 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,16 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n = -n;
 		_putchar('-');
 	}
 	else
 	{
-		n1 = n;
+		n = n;
 	}
-	if (n1 / 10)
+	if (n / 10)
 	{
-		print_number(n1 / 10);
+		print_number(n / 10);
 	}
-	putchar((n1 % 10) + '0');
+	_putchar((n % 10) + '0');
 }
