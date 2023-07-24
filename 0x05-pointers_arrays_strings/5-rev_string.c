4a978c369baa9bcccc6760b794e4e3c0f7631a3e
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-24 09:39:43 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,16 @@
 void rev_string(char *s)
 {
 	int length = strlen(s);
-	int i = length - 1;
+	int i = 0;
+	int j = length - 1;
 
-	while (i >= 0)
+	while (i < j)
 	{
-		putchar(s[i]);
-		i--;
+		char temp = s[i];
+
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
 	}
-	putchar('\n');
 }
