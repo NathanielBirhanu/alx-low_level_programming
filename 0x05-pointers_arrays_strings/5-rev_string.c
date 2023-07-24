9d102e08f29f2a989090072a0f634f5147b96f24
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-24 09:22:47 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @s: character parameter
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int i = length - 1;
+
+	while (i >= 0)
+	{
+		putchar(s[i]);
+		i--;
+	}
+}
