789eebcd100aceba9db31f54b31cfb945fd4c954
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-25 06:16:14 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the string to convert.
+ *
+ * Return: the integer value of the string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int digit;
+
+	while (*s == ' ' || *s == '\t' || *s == '\n' ||
+			*s == '\v' || *s == '\f' || *s == '\r')
+	{
+		s++;
+	}
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		sign = 1;
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		digit = *s - '0';
+
+		if (result > INT_MAX / 10 ||
+				(result == INT_MAX / 10 && digit > INT_MAX % 10))
+		{
+			/* Overflow detected */
+			return (sign == 1 ? INT_MAX : INT_MIN);
+		}
+
+		result = result * 10 + digit;
+		s++;
+	}
+
+	return (sign * result);
+}
