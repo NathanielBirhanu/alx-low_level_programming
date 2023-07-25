1eda25af1a7591b33a62cd2b9d7adbe9881360ff
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-25 07:14:37 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <limits.h>
-
 /**
  * _atoi - convert a string to an integer.
  * @s: the string to convert.
@@ -9,52 +8,19 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int digit;
-	int found_number = 0;
+	int sign = 1, result = 0, digit;
 
-	while (*s)
+	while (*s == ' ' || (*s >= '\t' && *s <= '\r'))
+		s++;
+	if (*s == '-' || *s == '+')
+		sign = (*s++ == '-') ? -1 : 1;
+	while (*s >= '0' && *s <= '9')
 	{
-		if (*s == '-')
-		{
-			sign = -sign;
-			s++;
-		}
-		else if (*s == '+')
-		{
-			s++;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			found_number = 1;
-
-			digit = *s - '0';
-
-			if (result > INT_MAX / 10 ||
-					(result == INT_MAX / 10 && digit > INT_MAX % 10))
-			{
-				/* Overflow detected */
-				return (sign == 1 ? INT_MAX : INT_MIN);
-			}
-
-			result = result * 10 + digit;
-			s++;
-		}
-		else
-		{
-			if (found_number)
-			{
-				/* We've seen at least one digit, so we're done */
-				break;
-			}
-			else
-			{
-				/* Skip non-digits at the start of the string */
-				s++;
-			}
-		}
+		digit = *s++ - '0';
+		if (result > INT_MAX / 10 ||
+				(result == INT_MAX / 10 && digit > INT_MAX % 10))
+			return ((sign == -1) ? INT_MIN : INT_MAX);
+		result = result * 10 + digit;
 	}
-
 	return (sign * result);
 }
