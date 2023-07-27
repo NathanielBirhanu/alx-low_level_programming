f1e7656c2be9d1766a5e623b1dfc282d6d87bf20
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-27 08:54:00 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,46 +12,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0;
-	int i, j;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	if (len1 + 1 > size_r || len2 + 1 > size_r)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		int digit1 = i >= 0 ? n1[i] - '0' : 0;
-		int digit2 = j >= 0 ? n2[j] - '0' : 0;
-		int sum = digit1 + digit2 + carry;
-
-		if (sum > 9)
-		{
-			carry = 1;
-			sum -= 10;
-		}
+		if (i < 0)
+			val1 = 0;
 		else
-		{
-			carry = 0;
-		}
-		if (i + j + 2 > size_r)
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
 			return (0);
-		r[i + j + 1] = sum + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (carry && len1 + len2 + 2 <= size_r)
-	{
-		r[0] = carry + '0';
-		r[len1 + len2 + 1] = '\0';
-		return (&r[0]);
-	}
-	else if (!carry)
-	{
-		r[len1 + len2] = '\0';
-		return (&r[1]);
-	}
-	else
-	{
+	if (digits == size_r)
 		return (0);
-	}
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
+
