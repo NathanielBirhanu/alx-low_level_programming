1fe10522dbf2f3f55695c881c4bd93913bcda52f
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-27 00:37:33 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,15 +9,14 @@
 char *leet(char *str)
 {
 	int i, j;
-	char leet_chars[] = "AEGOTL";
-	char leet_nums[] = "436017";
+	char leet_chars[] = "AaEeOoTtLl";
+	char leet_nums[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; leet_chars[j] != '\0'; j++)
 		{
-			if (str[i] == leet_chars[j] ||
-					str[i] == leet_chars[j] + 32)
+			if (str[i] == leet_chars[j])
 			{
 				str[i] = leet_nums[j];
 				break;
