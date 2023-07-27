e71abc9c1e84afc26d2cce4f8906e4b5981a0cc5
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-27 08:07:32 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,48 +12,46 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
-    int carry = 0;
-    int i, j;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int carry = 0;
+	int i, j;
 
-    if (len1 + 1 > size_r || len2 + 1 > size_r)
-        return 0;
+	if (len1 + 1 > size_r || len2 + 1 > size_r)
+		return (0);
 
-    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
-    {
-        int digit1 = i >= 0 ? n1[i] - '0' : 0;
-        int digit2 = j >= 0 ? n2[j] - '0' : 0;
-        int sum = digit1 + digit2 + carry;
+	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+	{
+		int digit1 = i >= 0 ? n1[i] - '0' : 0;
+		int digit2 = j >= 0 ? n2[j] - '0' : 0;
+		int sum = digit1 + digit2 + carry;
 
-        if (sum > 9)
-        {
-            carry = 1;
-            sum -= 10;
-        }
-        else
-        {
-            carry = 0;
-        }
-
-        if (i + j + 2 > size_r)
-            return 0;
-        r[i + j + 1] = sum + '0';
-    }
-
-    if (carry && len1 + len2 + 2 <= size_r)
-    {
-        r[0] = carry + '0';
-        r[len1 + len2 + 1] = '\0';
-        return &r[0];
-    }
-    else if (!carry)
-    {
-        r[len1 + len2] = '\0';
-        return &r[1];
-    }
-    else
-    {
-        return 0;
-    }
+		if (sum > 9)
+		{
+			carry = 1;
+			sum -= 10;
+		}
+		else
+		{
+			carry = 0;
+		}
+		if (i + j + 2 > size_r)
+			return (0);
+		r[i + j + 1] = sum + '0';
+	}
+	if (carry && len1 + len2 + 2 <= size_r)
+	{
+		r[0] = carry + '0';
+		r[len1 + len2 + 1] = '\0';
+		return (&r[0]);
+	}
+	else if (!carry)
+	{
+		r[len1 + len2] = '\0';
+		return (&r[1]);
+	}
+	else
+	{
+		return (0);
+	}
 }
