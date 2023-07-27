ac23c646e9a5009371484ccc71c3b2e29606a4b3
NathanielBirhanu <nbirhanu98@gmail.com>
2023-07-27 07:55:55 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,59 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * infinite_add - adds two numbers
+ * @n1: the first number to add
+ * @n2: the second number to add
+ * @r: the buffer to store the result
+ * @size_r: the size of the buffer
+ *
+ * Return: a pointer to the result, or 0 if the result cannot be stored in r
  */
-
-void rev_string(char *n)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int carry = 0;
+    int i, j;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
-	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
-		i--;
-	}
-	if (digits == size_r)
-		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r)
+    if (len1 + 1 > size_r || len2 + 1 > size_r)
+        return 0;
+
+    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+    {
+        int digit1 = i >= 0 ? n1[i] - '0' : 0;
+        int digit2 = j >= 0 ? n2[j] - '0' : 0;
+        int sum = digit1 + digit2 + carry;
+
+        if (sum > 9)
+        {
+            carry = 1;
+            sum -= 10;
+        }
+        else
+        {
+            carry = 0;
+        }
+
+        if (i + j + 2 > size_r)
+            return 0;
+        r[i + j + 1] = sum + '0';
+    }
+
+    if (carry && len1 + len2 + 2 <= size_r)
+    {
+        r[0] = carry + '0';
+        r[len1 + len2 + 1] = '\0';
+        return &r[0];
+    }
+    else if (!carry)
+    {
+        r[len1 + len2] = '\0';
+        return &r[1];
+    }
+    else
+    {
+        return 0;
+    }
 }
