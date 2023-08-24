463e3fdadc78319209ae865198551bf28ff41af2
NathanielBirhanu <nbirhanu98@gmail.com>
2023-08-24 12:48:07 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,23 +4,19 @@
  * print_list - Counts and prints the number of nodes in a linked list.
  * @h: Pointer to the head of the list.
  *
- * Return: The number of nodes in the list.
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
-        const list_t *current = h;
+    const list_t *current = h;
+    size_t count = 0;
 
-	if (h == NULL)
-	{
-		printf("[0] (nil)");
-		return (0);
-	}
-	while (current != NULL)
-	{
-		count++;
-		current = current->next;
-	}
-	printf("%lu", count);
-	return (count);
+    while (current != NULL)
+    {
+        printf("[%d] %s\n", current->len, current->str);
+        count++;
+        current = current->next;
+    }
+
+    return count;
 }
