2d5065849f74a14e6e8c6c57c6f0101bbf3be7d2
NathanielBirhanu <nbirhanu98@gmail.com>
2023-08-24 13:10:29 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,13 @@ size_t print_list(const list_t *h)
 {
 	const list_t *current = h;
 	size_t count = 0;
-	
+
 	while (current != NULL)
 	{
-		printf("[%d] %s\n", current->len, current->str);
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", current->len, current->str);
 		count++;
 		current = current->next;
 	}
