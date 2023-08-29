d8bc33167fec958aa56e9421a72f9564a774ddbb
NathanielBirhanu <nbirhanu98@gmail.com>
2023-08-29 10:36:50 -0700

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,37 +1,32 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list.
+ * free_listint_safe - Frees a listint_t list.
  * @h: Pointer to the head of the list.
  *
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *temp;
-	size_t count = 0;
+    listint_t *current, *next_node;
+    size_t count = 0;
 
-	if (h == NULL || *h == NULL)
-		return (0);
+    if (h == NULL)
+        return 0;
 
-	current = *h;
-	*h = NULL; /* Set the head to NULL to indicate an empty list */
+    current = *h;
+    while (current != NULL)
+    {
+        count++;
+        next_node = current->next;
+        free(current);
+        current = next_node;
+        if (current == *h)
+        {
+            *h = NULL;
+            break;
+        }
+    }
 
-	while (current != NULL)
-	{
-		count++;
-		temp = current;
-		current = current->next;
-		free(temp);
-
-		/* Check if the next node is already visited */
-		if (current == *h)
-		{
-			current = NULL;
-			break;
-		}
-	}
-
-	return (count);
+    return count;
 }
