0f4d461b28fde684a5ae96132dc87b20f2774689
NathanielBirhanu <nbirhanu98@gmail.com>
2023-11-01 11:11:08 -0700

    updated

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * Definition for doubly-linked list.
+ * struct dlistint_s {
+ *     int n;
+ *     struct dlistint_s *prev;
+ *     struct dlistint_s *next;
+ * };
+ * typedef struct dlistint_s dlistint_t;
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index) {
+    if (head == NULL)
+        return NULL;
+
+    dlistint_t *current = head;
+    unsigned int count = 0;
+
+    while (current != NULL && count < index) {
+        current = current->next;
+        count++;
+    }
+
+    if (current == NULL)
+        return NULL;
+
+    return current;
+}
